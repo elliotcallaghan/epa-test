0b9d456cb1ff2d6577a1ba0c6b685c6a267b0f71
elliotcallaghan <callaghan.elliot@yahoo.com>
2018-08-30 17:55:38 +0100

    Implemented pause function, and general bug fixes

--- epa-test.js
+++ epa-test.js
@@ -14,7 +14,7 @@ const canvas = document.getElementById("canvas"),
 let timer,
     currentTime,
     expected,
-    currentLevel = 0,
+    currentLevel,
     x,
     y;
 
@@ -22,14 +22,16 @@ let timer,
 $("#start").on("click", function () {
     $(this).hide();
     $(document).on("keydown", keyListener);
+    $(document).on("keydown", togglePause);
     drawEverything();
 });
 
 //reset position and timer
 $("#retry").on("click", function () {
+    $("input").hide();
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    $(this).hide();
+    $(document).on("keydown", keyListener);
     $(document).on("keydown", keyListener);
     $("#timer").text("Time remaining: 1:00");
     drawEverything();
@@ -37,9 +39,11 @@ $("#retry").on("click", function () {
 
 //back to menu
 $("#menu").on("click", function () {
-    $(this).hide();
+    clearTimeout(timer);
+    timer = null;
+    $("input").hide();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    $(document).off("keydown", keyListener);
+    $(document).off("keydown");
     $("#start").show();
     $("#timer").text("Time remaining: 1:00");
     loadMenu();
@@ -47,12 +51,14 @@ $("#menu").on("click", function () {
 
 //loads main menu
 function loadMenu() {
+    currentLevel = 0;
     $("#start").show();
     ctx.font = "45px serif";
+    ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillText("Maze Game", canvas.width / 2 - ctx.measureText("Maze Game").width / 2, canvas.height / 4);
     ctx.font = "20px serif";
-    ctx.fillText("There are three levels.",
-                 canvas.width / 2 - ctx.measureText("There are three levels.").width / 2, canvas.height * 0.4);
+    ctx.fillText("There are " + levels.length + " levels.",
+                 canvas.width / 2 - ctx.measureText("There are " + levels.length + " levels.").width / 2, canvas.height * 0.4);
     ctx.fillText("You have 1 minute to complete each.",
                  canvas.width / 2 - ctx.measureText("You have 1 minute to complete each.").width / 2, canvas.height / 2);
     ctx.fillText("Click start to begin.",
@@ -73,7 +79,7 @@ function drawEverything() {
         ctx.fillRect(x, y, rectWidth, rectHeight);
 
         //draw the goal
-        ctx.fillStyle = "rgba(0, 128, 0, .5)";
+        ctx.fillStyle = "rgb(127, 191, 127)";
         ctx.fillRect(levels[currentLevel].goalX, levels[currentLevel].goalY, 15, 15);
         
         //set the timer
@@ -93,6 +99,7 @@ function countdown() {
     --currentTime;
     expected += interval;
     clearTimeout(timer);
+    timer = null;
     timer = setTimeout(countdown, Math.max(0, interval - dt)); 
     
     if (currentTime.toString().length > 1) {
@@ -100,8 +107,10 @@ function countdown() {
     } else {
         $("#timer").text("Time remaining: 0:0" + currentTime);
         if (currentTime === 0) {
+            $(document).off("keydown");
             result();
             ctx.fillText("Time's up!", canvas.width / 2 - ctx.measureText("Time's up!").width / 2, canvas.height / 2);
+            $("#menu").show();
             $("#retry").show();
         }
     }
@@ -111,10 +120,9 @@ function countdown() {
 function checkCollision(newX, newY) {
     if (newX > 0 && newY > 0 && newX < (canvas.width - rectWidth) && newY < (canvas.height - rectHeight)) {
         let pixels = ctx.getImageData(newX, newY, rectWidth, rectHeight).data,
-            collision = 3,
-            i = 0;
+            collision = 3;
 
-        for (i; i < 4 * rectWidth * rectHeight; i += 4) {
+        for (let i = 0; i < 4 * rectWidth * rectHeight; i += 4) {
             if (pixels[i] === 0 && pixels[i + 1] === 0 && pixels[i + 2] === 0) {
                 collision = 1; //moving into a wall
                 break;
@@ -137,6 +145,7 @@ function checkCollision(newX, newY) {
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
                     drawEverything();
                 } else {
+                    $(document).off("keydown");
                     result();
                     ctx.fillText("Success!", canvas.width / 2 - ctx.measureText("Success!").width / 2, canvas.height / 2);
                     $("#menu").show();
@@ -146,10 +155,34 @@ function checkCollision(newX, newY) {
     }
 }
 
+//if timer is running, pauses it, otherwise restarts it
+function togglePause(e) {
+    if (e.key === "Escape" || e.key === "p") {
+        if (timer === null) {
+            $("input").hide();
+            $(document).on("keydown", keyListener);
+            drawEverything();
+            expected = Date.now() + interval;
+            countdown();
+        } else {
+            $(document).off("keydown", keyListener);
+            clearTimeout(timer);
+            timer = null;
+            ctx.fillStyle = "rgb(255, 255, 255)";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.font = "35px serif";
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillText("Paused", canvas.width / 2 - ctx.measureText("Paused").width / 2, canvas.height / 2);
+            $("#menu").show();
+            $("#retry").show();
+        }
+    }
+}
+
 //prepare success or fail screen
 function result() {
-    $(document).off("keydown", keyListener);
     clearTimeout(timer);
+    timer = null;
     ctx.fillStyle = "rgba(255, 255, 255, .8)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.font = "35px serif";
@@ -159,22 +192,22 @@ function result() {
 //directional buttons and wasd key event listeners
 function keyListener(e) {
     switch (e.key) {
-    case "ArrowUp":
-    case "w":
-        checkCollision(x, y - 5);
-        break;
-    case "ArrowDown":
-    case "s":
-        checkCollision(x, y + 5);
-        break;
-    case "ArrowLeft":
-    case "a":
-        checkCollision(x - 5, y);
-        break;
-    case "ArrowRight":
-    case "d":
-        checkCollision(x + 5, y);
-        break;
+        case "ArrowUp":
+        case "w":
+            checkCollision(x, y - 5);
+            break;
+        case "ArrowDown":
+        case "s":
+            checkCollision(x, y + 5);
+            break;
+        case "ArrowLeft":
+        case "a":
+            checkCollision(x - 5, y);
+            break;
+        case "ArrowRight":
+        case "d":
+            checkCollision(x + 5, y);
+            break;
     }
 }
 
