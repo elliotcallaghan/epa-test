aa4f70ae8d25fd4a49081c2dd62fec5f2e315625
elliotcallaghan <callaghan.elliot@yahoo.com>
2018-08-31 17:55:05 +0100

    Fixed timer delay between levels

--- epa-test.js
+++ epa-test.js
@@ -12,7 +12,7 @@ const canvas = document.getElementById("canvas"),
       ];
 
 let timer,
-    currentTime,
+    currentTime = 60,
     expected,
     currentLevel,
     x,
@@ -20,7 +20,7 @@ let timer,
 
 //show maze and start timer
 $("#start").on("click", function () {
-    $(this).hide();
+    $("#start").hide();
     $(document).on("keydown", keyListener);
     $(document).on("keydown", togglePause);
     drawEverything();
@@ -33,18 +33,20 @@ $("#retry").on("click", function () {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     $(document).on("keydown", keyListener);
     $(document).on("keydown", keyListener);
+    currentTime = 60;
     $("#timer").text("Time remaining: 1:00");
     drawEverything();
 });
 
 //back to menu
 $("#menu").on("click", function () {
-    clearTimeout(timer);
-    timer = null;
     $("input").hide();
+    $("#start").show();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     $(document).off("keydown");
-    $("#start").show();
+    clearTimeout(timer);
+    timer = null;
+    currentTime = 60;
     $("#timer").text("Time remaining: 1:00");
     loadMenu();
 });
@@ -83,10 +85,8 @@ function drawEverything() {
         ctx.fillRect(levels[currentLevel].goalX, levels[currentLevel].goalY, 15, 15);
         
         //set the timer
-        $("#timer").text("Time Remaining: 1:00");
-        currentTime = 60;
         expected = Date.now() + interval;
-        countdown();
+        setTimeout(countdown, interval);
     };
     mazeImage.crossOrigin = "anonymous";
     mazeImage.src = levels[currentLevel].image;
@@ -100,8 +100,8 @@ function countdown() {
     expected += interval;
     clearTimeout(timer);
     timer = null;
-    timer = setTimeout(countdown, Math.max(0, interval - dt)); 
-    
+    timer = setTimeout(countdown, Math.max(0, interval - dt));
+
     if (currentTime.toString().length > 1) {
         $("#timer").text("Time remaining: 0:" + currentTime);
     } else {
@@ -110,8 +110,7 @@ function countdown() {
             $(document).off("keydown");
             result();
             ctx.fillText("Time's up!", canvas.width / 2 - ctx.measureText("Time's up!").width / 2, canvas.height / 2);
-            $("#menu").show();
-            $("#retry").show();
+            $("#menu, #retry").show();
         }
     }
 }
@@ -141,8 +140,12 @@ function checkCollision(newX, newY) {
             ctx.fillRect(x, y, rectWidth, rectHeight);
             if (collision === 2) { //goal reached
                 if (currentLevel + 1 !== levels.length) { //if not on last level
+                    clearTimeout(timer);
+                    timer = null;
                     ++currentLevel;
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    $("#timer").text("Time remaining: 1:00");
+                    currentTime = 60;
                     drawEverything();
                 } else {
                     $(document).off("keydown");
@@ -162,8 +165,6 @@ function togglePause(e) {
             $("input").hide();
             $(document).on("keydown", keyListener);
             drawEverything();
-            expected = Date.now() + interval;
-            countdown();
         } else {
             $(document).off("keydown", keyListener);
             clearTimeout(timer);
@@ -173,8 +174,7 @@ function togglePause(e) {
             ctx.font = "35px serif";
             ctx.fillStyle = "rgb(0, 0, 0)";
             ctx.fillText("Paused", canvas.width / 2 - ctx.measureText("Paused").width / 2, canvas.height / 2);
-            $("#menu").show();
-            $("#retry").show();
+            $("#menu, #retry").show();
         }
     }
 }
